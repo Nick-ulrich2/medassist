49bba5567652fb6a9383c8ad9b7aea2f8870b36c
MVONGO NFOTO Hudes Ulrich <ulrichmvongo9@gmail.com>
2026-02-23 23:20:50 +0100

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 
+import { Button } from "./components/ui/Button";
 import Login from "./pages/Login";
+import FirstPage from "./pages/registerPages/FirstPage";
 
 function App() {
     return (
         <div>
-            {/* <Login/> */}
-            <button className="btn btn-neutral">Neutral</button>
-            <button className="btn btn-primary">Primary</button>
-            <button className="btn btn-secondary">Secondary</button>
-            <button className="btn btn-accent">Accent</button>
-            <button className="btn btn-info">Info</button>
-            <button className="btn btn-success">Success</button>
-            <button className="btn btn-warning">Warning</button>
-            <button className="btn btn-error">Error</button>
+            <Login/>
+            {/* <FirstPage/> */}
         </div>
     )
 };
